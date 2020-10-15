666514d49a4acfdfd97d1bbaeb891fd4a959da34
alex <alex.ghose@generalassemb.ly>
2020-10-15 12:41:09 +0200

    task 2 starter code

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET products listing. */
 router.get('/', function(req, res, next) {
-  res.send('welcome to amazon');
+  let products = ["lg big screen tv", "iphone x", "bose bluetooth earbuds"]
+  res.send('welcome to amazon. our products are:' + products);
 });
 
 module.exports = router;
